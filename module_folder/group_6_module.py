da9c4fe7802e4ddc5ffa0e2a42c35ff2a8255615
Toby-masuku <tobymasuku94@gmail.com>
2020-02-18 11:49:05 +0200

    Toby made changes to function 1 and 3

--- module_folder/group_6_module.py
+++ module_folder/group_6_module.py
@@ -1,4 +1,21 @@
 #functions to be saved here
+#Function 1
+def dictionary_of_metrics(list):
+    mean=round(np.mean(list),2)
+    median=round(np.percentile(list,50),2)
+    var=round(np.var(list,ddof=1),2)
+    std=round(np.std(list,ddof=1),2)
+    mini=round(min(list),2)
+    maxi=round(max(list),2)
+
+    keys=['mean','median','var','std','min','max']
+    values=[mean,median,var,std,mini,maxi]
+
+    dict ={} #created a dictionary
+    for l in range(len(keys)):
+        dict[keys[l]]=values[l]
+
+    return (dict)
 #function 2
 def five_num_summary(items):
    
@@ -49,3 +66,7 @@ def stop_words_remover(df):
     df_new['Without Stop Words'] = tokens
 
     return df_new
+#Function 3
+def date_parser(dates):
+    parsed_dates=[i.split()[0] for i in dates]
+    return(parsed_dates)
