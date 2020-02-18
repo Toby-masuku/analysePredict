6787e50235616ae5a2b56e3c8502728511d9a8e0
Marcio Maluka <marciomaluka@ymail.com>
2020-02-18 10:04:05 +0200

    Added function 7 to module

--- module_folder/group_6_module.py
+++ module_folder/group_6_module.py
@@ -12,3 +12,18 @@ def five_num_summary(items):
 def word_splitter(df):
     df['Split Tweets']=df['Tweets'].str.lower().str.split(" ")
     return (df)
+
+#Function 4:
+def extract_municipality_hashtags(df):
+    df['municipality']=np.nan
+    df['hashtags']=df.Tweets.str.split(' ')
+    def fn(row):
+        row.municipality=[mun_dict[i] for i in row.hashtags if i in mun_dict.keys()] #list of the words with @ in them
+        row.hashtags=[i.lower() for i in row.hashtags if '#' in i]
+        if len(row.hashtags)==0:
+            row.hashtags=np.nan  
+        if len(row.municipality)==0:
+            row.municipality=np.nan
+        return(row)
+    df=df.apply(fn,axis=1)
+    return (df)
