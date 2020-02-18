02c9ced77a3ff0deaa687eb940437a04a4f462eb
phiwa-lab <60380888+phiwa-lab@users.noreply.github.com>
2020-02-18 09:55:35 +0200

    added function 2 and function 6

--- module_folder/group_6_module.py
+++ module_folder/group_6_module.py
@@ -1,1 +1,14 @@
-functions to be saved here
+#functions to be saved here
+#function 2
+def five_num_summary(items):
+   
+    return{'max':max(items),
+          'median':np.median(items),
+          'min':min(items),
+          'qi':np.quantile(items,q=0.25),
+          'q3':np.quantile(items,q=0.75)}
+#function 6
+
+def word_splitter(df):
+    df['Split Tweets']=df['Tweets'].str.lower().str.split(" ")
+    return (df)
