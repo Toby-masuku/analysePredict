918867b86b2b374942075493d4b4b8e6e7ab1136
Marcio Maluka <marciomaluka@ymail.com>
2020-02-19 16:59:29 +0200

    syntax error on setup

--- setup.py
+++ setup.py
@@ -1,21 +1,5 @@
 from setuptools import setup, find_packages
 
-# setup(
-#     name='Group_6_predict',
-#     version='0.1',
-#     packages=find_packages(exclude=['tests*']),
-#     license='MIT',
-#     description='This package extract tweets',
-#     long_description=open('README.md').read(),
-#     install_requires=['numpy','pandas'],
-#     url='https://github.com/Toby-masuku/analysePredict.git',
-#     authors='Christopher Mahlangu, Marcio Maluka, Phiwayinkosi Hlatshwayo, Toby Masuku, Tumisang Sentle',
-#     author_email='marciomaluka@ymail.com'
-# )
-
-
-# from setuptools import setup, find_packages
-
 setup(
     name='Group_6_predict',
     version='0.1',
@@ -23,9 +7,25 @@ setup(
     license='MIT',
     description='This package extract tweets',
     long_description=open('README.md').read(),
-    install_requires=['numpy'],
-    url='https://github.com/Toby-masuku/analysePredict',
-    author='marcio Maluka')
+    install_requires=['numpy','pandas'],
+    url='https://github.com/Toby-masuku/analysePredict.git',
+    authors='Christopher Mahlangu, Marcio Maluka, Phiwayinkosi Hlatshwayo, Toby Masuku, Tumisang Sentle',
+    author_email='marciomaluka@ymail.com'
+)
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='Group_6_predict',
+#     version='0.1',
+#     packages=find_packages(exclude=['tests*']),
+#     license='MIT',
+#     description='This package extract tweets',
+#     long_description=open('README.md').read(),
+#     install_requires=['numpy'],
+#     url='https://github.com/Toby-masuku/analysePredict',
+#     author='marcio Maluka')
 
 
 
