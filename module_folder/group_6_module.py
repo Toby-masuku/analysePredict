f5d0474bcc217049299566b77c3f3add14f27edb
Marcio Maluka <marciomaluka@ymail.com>
2020-02-19 11:08:05 +0200

    Added function 4 Docstring, and formated document

--- module_folder/group_6_module.py
+++ module_folder/group_6_module.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+#Function 1
 def dictionary_of_metrics(list):
     """
     This functions give the maximum, minimum, median, 1st quartile of a given list as a dictionary:
@@ -30,55 +31,92 @@ def dictionary_of_metrics(list):
     for l in range(len(keys)):
         dict[keys[l]]=values[l]
     return (dict)
-    
+
 #Function 3
-"""removes time from the date
+
+def date_parser(dates):
+    """
+    removes time from the date
     eg input: dates[:3] == [
     '2019-11-29 12:50:54',
     '2019-11-29 12:46:53',
     '2019-11-29 12:46:10']
 
     output: ['2019-11-29', '2019-11-29', '2019-11-29']
-  
-"""
-def date_parser(dates):
 
-### create a new list with a list comprehension that split time from the date
+    """
+
+#Create a new list with a list comprehension that split time from the date
     parsed_dates=[i.split()[0] for i in dates]
     return(parsed_dates)
 
-#function2
-def five_num_summary(items):
-   
-    return{'max':max(items),
-          'median':np.median(items),
-          'min':min(items),
-          'qi':np.quantile(items,q=0.25),
-          'q3':np.quantile(items,q=0.75)}
 
+#function 2
+def five_num_summary(list):
+    """
+    This functions give the maximum, minimum, median, 1st quartile of a given list as a dictionary:
+
+    input:
+    fn([1,2,3])
+    ___________________
+
+    Output:
+    [1,2,3]
+
+    {max:3.0, min: 1.0, median:2.0, q1: 1.5, q3: 2.5}
+    """
+    # create dictionary for five number summary
+    dict={'max':round(np.max(list),2),
+    'min':round(np.min(list),2),
+    'median':round(np.percentile(list,50),2),
+    'q1':round(np.percentile(list,25),2),
+    'q3':round(np.percentile(list,75),2)}
+    
+    return (dict)
+    
 #function 6
 def word_splitter(df):
+    """
+    This function splits the sentences in dataframe's column into list of separate words
+    """
     df['Split Tweets']=df['Tweets'].str.lower().str.split(" ")
     return (df)
 
 #Function 5
+
 def number_of_tweets_per_day(df):
+    '''
+    This function takes a pandas dataframe as input.
+    It then returns a new dataframe, grouped by day, with the number of tweets for that day.
+    '''
+    #Function removes time values from the dates' column
     def fn (row):
         row.Date=row.Date.split()[0]
         return(row)
+    #It slices column's dates and tweets
     df_sliced=df.loc[:,['Date','Tweets']].apply(fn,axis=1)
-    
+    #Use pd.datetime to change date into datetime object
     df_sliced.Date=pd.to_datetime(df_sliced.Date,format='%Y-%m-%d')
     df_count=df_sliced.groupby('Date').count()
     return (df_count)
     
 #Function 4:
 def extract_municipality_hashtags(df):
+    """ This is a function which extracts Tweets and Municipality names from a tweet.
+     ---------------------------------------------------------------------------
+     1. It  take a pandas dataframe as input.
+     2. The function extracts the municipality twitter usernames and maps them to the municipal dictionary keys.
+     3. The function then extracts all hashtags in the 'Tweets Column' and stores the result as a list in Hashtags column.
+     4. Function modifies the DataFrame
+     5. Finally the Function will return the modified Data Frame
+     --------------------------------------------------------------------------- 
+     """
+
     df['municipality']=np.nan
     df['hashtags']=df.Tweets.str.split(' ')
     def fn(row):
-        row.municipality=[mun_dict[i] for i in row.hashtags if i in mun_dict.keys()] #list of the words with @ in them
-        row.hashtags=[i.lower() for i in row.hashtags if '#' in i]
+        row.municipality=[mun_dict[i] for i in row.hashtags if i in mun_dict.keys()] #Municipality @ extracted to 'Municipality' column
+        row.hashtags=[i.lower() for i in row.hashtags if '#' in i]                   #Extraction of hashtags
         if len(row.hashtags)==0:
             row.hashtags=np.nan  
         if len(row.municipality)==0:
@@ -87,9 +125,10 @@ def extract_municipality_hashtags(df):
     df=df.apply(fn,axis=1)
     return (df)
 
-    ## Function 7
-    
-"""" This is a function which removes english stop words from a tweet.
+# Function 7
+
+def stop_words_remover(df):
+    """ This is a function which removes english stop words from a tweet.
      ---------------------------------------------------------------------------
      1. It  take a pandas dataframe as input.
      2. It  tokenise the sentences according to the definition in function 6.
@@ -97,9 +136,9 @@ def extract_municipality_hashtags(df):
      4. "Without Stop Words" stores the localized column.
      5. Function modifies the DataFrame
      6. Finally the Function will return the modified Data Frame
-     --------------------------------------------------------------------------- """"
+     --------------------------------------------------------------------------- 
+     """
 
-def stop_words_remover(df):
     # copy df into a new data frame: df_new
     df_new = df.copy()
 
--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find packages
 
 setup(
-    name='my_package',
+    name='Group_6_predict',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA example python package',
+    description='This package extract tweets',
     long_description=open('README.md').read(),
-    install_requires=['numpy'],
+    install_requires=['numpy'],['pandas]
     url='https://github.com/<username>/<package-name>',
-    author='Marcio',
-    author_email='marciomaluka@ymail.com'
-)
+    authors='Christopher Mahlangu','Marcio Maluka','Phiwayinkosi Hlatshwayo','Toby Masuku','Tumisang Sentle'
